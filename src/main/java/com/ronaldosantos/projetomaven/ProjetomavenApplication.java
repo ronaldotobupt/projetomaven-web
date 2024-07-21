f1f3cf847b133b7f38742a983dc75b418331639f
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2024-07-21 12:23:53 -0300

    Projeto criado

---
+++ src/main/java/com/ronaldosantos/projetomaven/ProjetomavenApplication.java
@@ -0,0 +1,13 @@
+package com.ronaldosantos.projetomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronaldosantos/projetomaven/ProjetomavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldosantos.projetomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
